2d5c645baf8d88d36a72ed33fa00424745581672
luochengleo <luochengleo@gmail.com>
2014-07-03 14:31:04 +0800

    2

--- query2vec/clueweb12/ExtractEnPage.py
+++ query2vec/clueweb12/ExtractEnPage.py
@@ -16,20 +16,20 @@ for folder in os.listdir(foldername):
     for filename in os.listdir(subfoldername):
         print subfoldername,filename
         os.system('7z e '+subfoldername+'/'+filename)
-    warcfilename = filename.replace('.7z','')
-    f = warc.open(warcfilename)
-
-    for record in f:
-        id = record.header.get('WARC-TREC-ID')
-        if id in pages:
-            fout = open('../data/parse/'+str(record.header.get('WARC-TREC-ID')+'.html'),'w')
-            content = record.payload.read()
-            write = False
-            for l in content.split('\n'):
-                if '<!DOCTYPE' in l:
-                    write=True
-                if write == True:
-                    fout.write(l+'\n')
-            fout.write(content)
-            fout.close()
-    os.system('rm '+warcfilename)
+        warcfilename = filename.replace('.7z','')
+        f = warc.open(warcfilename)
+    
+        for record in f:
+            id = record.header.get('WARC-TREC-ID')
+            if id in pages:
+                fout = open('../data/parse/'+str(record.header.get('WARC-TREC-ID')+'.html'),'w')
+                content = record.payload.read()
+                write = False
+                for l in content.split('\n'):
+                    if '<!DOCTYPE' in l:
+                        write=True
+                    if write == True:
+                        fout.write(l+'\n')
+                fout.write(content)
+                fout.close()
+        os.system('rm '+warcfilename)
