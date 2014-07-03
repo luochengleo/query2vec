77bceba4ffe0b38809074c96a326ce6f39e8a27e
luochengleo <luochengleo@gmail.com>
2014-07-03 16:40:49 +0800

    afa

--- query2vec/sogout08/FindWebPages.py
+++ query2vec/sogout08/FindWebPages.py
@@ -1,6 +1,11 @@
 #coding=cp936
 import os
+import sys
+
 docs  = set()
+
+mod = int(sys.argv[1])
+
 
 for l in open('../ref/docs.txt').readlines():
     docs.add(l.strip())
@@ -10,31 +15,33 @@ print len(docs),' docs need to find'
     
 files = os.listdir('/work/caoyj/SogouT')
 for f in files:
+    
     if '.7z' in f:
-        doccount =0
-        datacontent = os.popen('7za e -so /work/caoyj/SogouT/'+f)
-        l  = datacontent.readline()
-        
-        write = False
-        fout = open('../data/temp.txt','w')
-        while l !='':
-            if write==True:
-                fout.write(l)
-                if '</DOC>' in l:
-                    write=False
-            else:
-                if '<DOCNO>' in l :
-                    doccount +=1
-                    docno = l.strip().replace('<DOCNO>','').replace('</DOCNO>','')
-                    print docno
-                    if docno in docs:
-                        print f,doccount,docno
-                        fout = open('../data/page/'+docno+'.html','w')
-                        fout.write('<DOC>\n')
-                        fout.write(l)
-                        write = True
-                
-            l = datacontent.readline()
+        idx = int(f.split('.')[1])
+        if idx%12==mod:
+            doccount =0
+            datacontent = os.popen('7za e -so /work/caoyj/SogouT/'+f)
+            l  = datacontent.readline()
+            
+            write = False
+            fout = open('../data/temp.txt','w')
+            while l !='':
+                if write==True:
+                    fout.write(l)
+                    if '</DOC>' in l:
+                        write=False
+                else:
+                    if '<DOCNO>' in l :
+                        doccount +=1
+                        docno = l.strip().replace('<DOCNO>','').replace('</DOCNO>','')
+                        if docno in docs:
+                            print f,doccount,docno
+                            fout = open('../data/page/'+docno+'.html','w')
+                            fout.write('<DOC>\n')
+                            fout.write(l)
+                            write = True
+                    
+                l = datacontent.readline()
             
                 
             
---
+++ query2vec/sogout08/run.sh
@@ -0,0 +1,13 @@
+nohup python FindWebPages.py 00 > 00.log & 
+nohup python FindWebPages.py 01 > 01.log & 
+nohup python FindWebPages.py 02 > 02.log & 
+nohup python FindWebPages.py 03 > 03.log & 
+nohup python FindWebPages.py 04 > 04.log & 
+nohup python FindWebPages.py 05 > 05.log & 
+nohup python FindWebPages.py 06 > 06.log & 
+nohup python FindWebPages.py 07 > 07.log & 
+nohup python FindWebPages.py 08 > 08.log & 
+nohup python FindWebPages.py 09 > 09.log & 
+nohup python FindWebPages.py 10 > 10.log & 
+nohup python FindWebPages.py 11 > 11.log & 
+
