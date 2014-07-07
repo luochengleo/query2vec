4357c24dde3b9bd51cba08e8dff939e40ca15227
luochengleo <luochengleo@gmail.com>
2014-07-07 17:24:46 +0800

    afa

--- query2vec/sogout08/FindMoreWebPages.py
+++ query2vec/sogout08/FindMoreWebPages.py
@@ -13,13 +13,13 @@ for l in open('../ref/docs.txt').readlines():
 
 print len(docs),' docs need to find'
     
-files = os.listdir('/work/caoyj/SogouT')
+files = os.listdir('/home/cluo/publicdata/sogout08/')
 for f in files:
     if '.7z' in f:
         idx = int(f.split('.')[1])
-        if idx%12==mod:
+        if idx%32==mod:
             doccount =0
-            datacontent = os.popen('7za e -so /work/caoyj/SogouT/'+f)
+            datacontent = os.popen('7za e -so /home/cluo/publicdata/sogout08/'+f)
             l  = datacontent.readline()
             
             write = False
