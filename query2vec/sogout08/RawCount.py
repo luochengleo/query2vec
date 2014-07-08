2fa3d8e2e72e74f50f6282ef3bceb6e86ae11253
luochengleo <luochengleo@gmail.com>
2014-07-08 14:32:26 +0800

    faf

---
+++ query2vec/sogout08/RawCount.py
@@ -0,0 +1,39 @@
+#coding=cp936
+import os
+import sys
+
+docs  = set()
+
+mod = int(sys.argv[1])
+
+
+for l in open('../ref/docs.txt').readlines():
+    docs.add(l.strip())
+    print l.strip()
+
+print len(docs),' docs need to find'
+
+files = os.listdir('/home/cluo/publicdata/sogout08/')
+fout = open('../data/count/'+str(mod)+'.count','w')
+for f in files:
+    if '.7z' in f:
+        idx = int(f.split('.')[1])
+        if idx%32==mod:
+            doccount =0
+            datacontent = os.popen('7za e -so /home/cluo/publicdata/sogout08/'+f)
+            l  = datacontent.readline()
+            
+            write = False
+            fout = open('../data/temp.txt','w')
+            count = 0
+            while l !='':
+                count +=1
+                for item  in docs:
+                    if item in l:
+                        fout.write('Find '+ item +' in the '+str(count)+ ' file '+f+'\n')
+                        
+                l = datacontent.readline()
+
+            
+                
+            
